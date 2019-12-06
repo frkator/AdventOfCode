fa980ce6d48fd3f73235db6a3e7cd4ce6e67b476
isipka <isipka@cartrawler.com>
2019-12-06 14:17:23 +0000

    day 5

--- src/main/kotlin/com/adventofcode/_2019/day/_5/extendedintcodecomputer/ExtendedIntCodeComputer.kt
+++ src/main/kotlin/com/adventofcode/_2019/day/_5/extendedintcodecomputer/ExtendedIntCodeComputer.kt
@@ -9,28 +9,43 @@ enum class LoadMode {
     IMMEDIATE
 }
 
-enum class Operation(val code:Int, val parameterCount:Int) : Function<List<Int>,Int> {
-    ADD(1, 3) {
+enum class Operation(val code:Int, val parameterCount:Int, val regular:Boolean) : Function<List<Int>,Int> {
+    ADD(1, 3, true) {
         override fun apply(operand:List<Int>): Int = operand[0] + operand[1]
     },
-    MULTIPLY(2, 3) {
+    MULTIPLY(2, 3,true) {
         override fun apply(operand:List<Int>): Int  = operand[0] * operand[1]
     },
-    INPUT (3,1) {
+    INPUT (3,1,true) {
         override fun apply(operand: List<Int>): Int {
             println("input:")
             return readLine()!!.toInt()
         }
     },
-    OUTPUT (4,1) {
+    OUTPUT (4,1,true) {
         override fun apply(operand: List<Int>): Int = operand[0]
     },
-    EXIT(99, -1) {
+    JUMP_IF_TRUE(5,2,false) {
+        override fun apply(operand: List<Int>): Int =  if (operand[0] > 0) { operand[1] } else { -1 }
+
+    },
+    JUMP_IF_FALSE(6,2,false) {
+        override fun apply(operand: List<Int>): Int =  if (operand[0] == 0) { operand[1] } else { -1 }
+
+    },
+    LESS_THAN(7,3,true) {
+        override fun apply(operand: List<Int>): Int =  if (operand[0] < operand[1]) { 1 } else { 0 }
+
+    },
+    EQUALS(8,3,true) {
+        override fun apply(operand: List<Int>): Int = if (operand[0] == operand[1]) { 1 } else { 0 }
+    },
+    EXIT(99, -1,false) {
         override fun apply(operand:List<Int>): Int {
             throw IllegalStateException("should never ever happen")
         }
     },
-    ILLEGAL(-1, -1) {
+    ILLEGAL(-1, -1,false) {
         override fun apply(operand:List<Int>): Int {
             throw IllegalStateException("should never happen")
         }
@@ -44,17 +59,20 @@ enum class Operation(val code:Int, val parameterCount:Int) : Function<List<Int>,
                         .find { operation ->
                             operation.code == code.toString().takeLast(2).toInt()
                         } ?: ILLEGAL
-                val loadMode = code
-                        .toString()
-                        .padStart(operation.parameterCount + 2, '0')
-                        .substring(0, operation.parameterCount)
-                        .map {
-                            when (it.toString().toInt()) {
-                                0 -> LoadMode.POSITION
-                                else -> LoadMode.IMMEDIATE
+                val loadMode = if (operation.parameterCount > 0) {
+                            code
+                            .toString()
+                            .padStart(operation.parameterCount + 2, '0')
+                            .substring(0, operation.parameterCount)
+                            .map {
+                                when (it.toString().toInt()) {
+                                    0 -> LoadMode.POSITION
+                                    else -> LoadMode.IMMEDIATE
+                                }
                             }
-                        }
-                        .toTypedArray()
+                            .reversed()
+                            .toTypedArray()
+                } else { arrayOf() }
                 return operation to loadMode
             }
             catch (e:Exception) {
@@ -66,7 +84,7 @@ enum class Operation(val code:Int, val parameterCount:Int) : Function<List<Int>,
 }
 
 
-data class Instruction(val programCounter:Int, val operation:Operation, val loadMode: Array<LoadMode>) {
+data class Instruction(val programCounter:ProgramCounter, val operation:Operation, val loadMode: Array<LoadMode>) {
 
     private fun load(code: Array<Int>, loadModeIndex:Int, address:Int):Int {
         return when(loadMode[loadModeIndex]) {
@@ -76,35 +94,62 @@ data class Instruction(val programCounter:Int, val operation:Operation, val load
     }
 
     fun execute(code: Array<Int>) {
-        val firstParameterAddress = programCounter + 1
-        val lastParameterAddress = programCounter + operation.parameterCount
+        val firstParameterAddress = programCounter.read() + 1
+        val lastParameterAddress = programCounter.read() + operation.parameterCount
         val resultAddress = code[lastParameterAddress]
         val parameterValues = (firstParameterAddress .. lastParameterAddress).map {
                                                 load(code,it-firstParameterAddress,it)
                                         }
         print("${parameterValues.take(operation.parameterCount - 1)} ")
-        code[resultAddress] = operation.apply(parameterValues)
-        println("writes ${resultAddress}=${code[resultAddress]}")
+        if (operation.regular) {
+            code[resultAddress] = operation.apply(parameterValues)
+            println("writes to memory @ address ${resultAddress} value ${code[resultAddress]}")
+            programCounter.increase(operation.parameterCount + 1)
+        }
+        else {
+            val result = operation.apply(parameterValues)
+            if (result > -1) {
+                println("jumping at $result ")
+                programCounter.write(result)
+            }
+            else {
+                println("no operation")
+                programCounter.increase(operation.parameterCount + 1)
+            }
+
+        }
     }
+}
+
+data class ProgramCounter(private var value:Int) {
+
+    fun increase(value:Int) {
+        this.value += value
+    }
+
+    fun write(value:Int) {
+        this.value = value
+    }
+
+    fun read():Int = value
 }
 
 class ExtendedIntCodeComputer(private val code:Array<Int>) {
 
     fun execute() {
-        var programCounter = 0
+        val programCounter = ProgramCounter(0)
         do {
-            print("PC=$programCounter: ")
-            val (operation,loadMode) = Operation.decode(code[programCounter])
+            print("PC=$programCounter: ${code[programCounter.read()]} ")
+            val (operation,loadMode) = Operation.decode(code[programCounter.read()])
             println("code=${operation.code} ${operation.name} load mode=${loadMode.toList()}")
             println (code.withIndex().groupBy { it.index }.toMap().mapValues { it.value.first().value })
             when(operation) {
                 Operation.ILLEGAL -> throw IllegalStateException()
                 Operation.EXIT -> {}
                 else -> {
-                   Instruction(programCounter, operation, loadMode).execute(code)
+                    val programCounterIncrease = Instruction(programCounter, operation, loadMode).execute(code)
                 }
             }
-            programCounter += (operation.parameterCount + 1)
             println("--end")
         }
         while(Operation.EXIT != operation)
@@ -115,6 +160,7 @@ class ExtendedIntCodeComputer(private val code:Array<Int>) {
 fun main(args:Array<String>) {
     val input = ExtendedIntCodeComputer::class.java.getResource("/com/adventofcode/_2019/day/_5/extendedintcodecomputer/source.txt")
     val source = input.readText().split(Regex(",")).map { it.toInt() }.toTypedArray()
+    //val source = arrayOf(3,0,4,0,99)
     val fileInput = source.copyOf()
     ExtendedIntCodeComputer(fileInput).execute()
     println(fileInput.toList())
---
+++ src/test/kotlin/com/adventofcode/_2019/day/_5/extendedintcodecomputer/ExtendedIntCodeComputerTest.kt
@@ -0,0 +1,53 @@
+package com.adventofcode._2019.day._5.extendedintcodecomputer
+
+import com.adventofcode._2019.day._2.intcodecomputer.IntCodeComputer
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class ExtendedIntCodeComputerTest {
+
+    companion object {
+        @JvmStatic
+        fun sourceCode() = listOf(
+                Arguments.of(
+                        arrayOf(1,9,10,3,2,3,11,0,99,30,40,50),
+                        arrayOf(3500,9,10,70,2,3,11,0,99,30,40,50)
+                ),
+                Arguments.of(
+                        arrayOf(1,0,0,0,99),
+                        arrayOf(2,0,0,0,99)
+                ),
+                Arguments.of(
+                        arrayOf(2,3,0,3,99),
+                        arrayOf(2,3,0,6,99)
+                ),
+                Arguments.of(
+                        arrayOf(2,4,4,5,99,0),
+                        arrayOf(2,4,4,5,99,9801)
+                ),
+                Arguments.of(
+                        arrayOf(1,1,1,4,99,5,6,0,99),
+                        arrayOf(30,1,1,4,2,5,6,0,99)
+                ),
+                Arguments.of(
+                        arrayOf(1002,4,3,4,33),
+                        arrayOf(1002, 4, 3, 4, 99)
+                ),
+                Arguments.of(
+                        arrayOf(1101,100,-1,4,0),
+                        arrayOf(1101, 100, -1, 4, 99)
+                )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceCode")
+    fun execute(input:Array<Int>,expectedResult:Array<Int>) {
+        ExtendedIntCodeComputer(input).execute()
+        assertArrayEquals(expectedResult,input)
+    }
+}
