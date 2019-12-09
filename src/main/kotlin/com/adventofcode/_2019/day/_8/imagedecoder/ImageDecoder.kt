287b6b5f6c58c2580711ebefed5a23231ff2df1f
isipka <isipka@cartrawler.com>
2019-12-09 12:12:49 +0000

    day 8

--- src/main/kotlin/com/adventofcode/_2019/day/_8/imagedecoder/ImageDecoder.kt
+++ src/main/kotlin/com/adventofcode/_2019/day/_8/imagedecoder/ImageDecoder.kt
@@ -1,27 +1,65 @@
 package com.adventofcode._2019.day._8.imagedecoder
 
+import java.lang.IllegalStateException
+
 
 class ImageDecoder(val input:String,val width:Int, val height:Int) {
 
-    val layerCount = width * height
+    val layerElementsCount = width * height
 
     fun decode(): List<String> {
-        check (input.length % layerCount == 0)
+        check (input.length % layerElementsCount == 0)
         val layers = mutableListOf<String>()
-        for (offset in 0..(input.length / layerCount)) {
-            layers.add(input.slice(offset..(offset + layerCount)) )
+        for (offset in 0 until input.length step layerElementsCount) {
+            layers.add(input.slice(offset until (offset + layerElementsCount)) )
         }
         return layers.toList()
     }
 
+    fun findLayerWithMinZeroes(layers:List<String>): Int {
+        return layers.withIndex().minBy { it.value.count { i-> i == '0' } }!!.index
+    }
+
     fun calculate(layers:List<String>):Int {
-        val layerWithMinZeroes = layers.minBy { it.count { i-> i == '0' } }
+        val layerWithMinZeroes = layers[findLayerWithMinZeroes(layers)]
         return layerWithMinZeroes!!.count { it == '2' } * layerWithMinZeroes!!.count { it == '1' }
+    }
+
+    fun compose(layers:List<String>): String {
+        val result = StringBuilder()
+        for (index in 0 until layerElementsCount) {
+            val nonZeroes = (0 until layers.size).map { layers[it].slice(index..index) }.filter { it != "2" }
+            result.append( if (nonZeroes.isEmpty()) { "2" } else { nonZeroes.first()})
+        }
+        return result.toString()
+    }
+
+    fun printComposedImageWithInvertedColors(image:String) {
+        println("image data")
+        image.windowed(width, width) {
+            println(it)
+        }
+        println("image")
+        image.windowed(width, width) { layer ->
+            layer.toString().forEach { pixel ->
+                print(
+                    when(pixel.toString()) {
+                        "0" -> " "
+                        "1" -> "█"
+                        "2" -> "x"
+                        else -> throw IllegalStateException()
+                    }
+                )
+            }
+            println()
+        }
     }
 }
 
 fun main(args:Array<String>) {
     val input = ImageDecoder::class.java.getResource("/com/adventofcode/_2019/day/_8/imagedecoder/image.txt")
     val decoder = ImageDecoder(input.readText(),25,6)
-    println(decoder.calculate(decoder.decode()))
+    val imageData = decoder.decode()
+    println(decoder.calculate(imageData))
+    decoder.printComposedImageWithInvertedColors(decoder.compose(imageData))
 }
--- src/test/kotlin/com/adventofcode/_2019/day/_8/imagedecoder/ImageDecoderTest.kt
+++ src/test/kotlin/com/adventofcode/_2019/day/_8/imagedecoder/ImageDecoderTest.kt
@@ -2,12 +2,57 @@ package com.adventofcode._2019.day._8.imagedecoder
 
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.aggregator.ArgumentAccessException
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class ImageDecoderTest {
 
-    @Test
-    fun test() {
-        val imageDecoder = ImageDecoder("1233456678990112", 4, 2)
-        println (imageDecoder.calculate(imageDecoder.decode()))
+    companion object {
+        @JvmStatic
+        fun imagesDecode(): List<Arguments> {
+            return listOf(
+                    Arguments.of(
+                            3,
+                            2,
+                            "123456789012",
+                            0,
+                            1
+                    )
+            )
+        }
+
+        @JvmStatic
+        fun imagesCompose(): List<Arguments> {
+            return listOf(
+                    Arguments.of(
+                            2,
+                            2,
+                            "0222112222120000",
+                            "0110"
+                    )
+            )
+        }
     }
+
+    @ParameterizedTest
+    @MethodSource("imagesDecode")
+    fun testDecode(width:Int,height:Int,image:String,expectedLayerIndex:Int, expectedResult:Int) {
+        val imageDecoder = ImageDecoder(image, width, height)
+        val layers = imageDecoder.decode()
+        assertEquals(expectedLayerIndex, imageDecoder.findLayerWithMinZeroes(layers),"wrong layer index")
+        assertEquals(expectedResult,imageDecoder.calculate(layers), "wrong result")
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("imagesCompose")
+    fun testCompose(width:Int,height:Int,image:String,expectedResult:String) {
+        val imageDecoder = ImageDecoder(image, width, height)
+        val layers = imageDecoder.decode()
+        val actualResult = imageDecoder.compose(layers)
+        assertEquals(expectedResult,actualResult, "wrong result")
+    }
+
 }
